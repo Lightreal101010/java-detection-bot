e880e1d08b7ae20f8e625d751615a9f68a8465b7
Lightreal101010 <sadinpepic4@gmail.com>
2026-04-16 15:34:36 +0200

    Update register.ts

--- src/commands/register.ts
+++ src/commands/register.ts
@@ -12,6 +12,14 @@ export async function registerCommands(client: Client<true>) {
       .setName('ticketpanel')
       .setDescription('Sends the ticket panel')
       .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild),
+
+    new SlashCommandBuilder()
+      .setName('staff')
+      .setDescription('Staff command'),
+
+    new SlashCommandBuilder()
+      .setName('ticket')
+      .setDescription('Ticket command'),
   ].map((cmd) => cmd.toJSON());
 
   const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_BOT_TOKEN!);
