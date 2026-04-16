33ff412333aec7e1a0d87d4aff05f6040c0eb5c0
Lightreal101010 <sadinpepic4@gmail.com>
2026-04-16 15:33:53 +0200

    Update Register.js

--- src/commands/Register.js
+++ src/commands/Register.js
@@ -1,11 +1,25 @@
-import { Client, REST, Routes, SlashCommandBuilder, PermissionFlagsBits } from 'discord.js';
+import {
+  Client,
+  PermissionFlagsBits,
+  REST,
+  Routes,
+  SlashCommandBuilder,
+} from 'discord.js';
 
 export async function registerCommands(client: Client<true>) {
   const commands = [
     new SlashCommandBuilder()
       .setName('ticketpanel')
-      .setDescription('Sendet das Ticket Panel')
+      .setDescription('Sends the ticket panel')
       .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild),
+
+    new SlashCommandBuilder()
+      .setName('staff')
+      .setDescription('Staff command'),
+
+    new SlashCommandBuilder()
+      .setName('ticket')
+      .setDescription('Ticket command'),
   ].map((cmd) => cmd.toJSON());
 
   const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_BOT_TOKEN!);
